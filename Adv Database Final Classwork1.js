c95413d4ac18da441b30ed06221809adb1cfe57c
nagellasrija <94693223+nagellasrija@users.noreply.github.com>
2024-01-31 14:26:27 -0500

    Advance Database Design week 1 with added comments

---
+++ Adv Database Final Classwork1.js
@@ -0,0 +1,86 @@
+// Create a collection named 'classwork'
+db.createCollection('classwork')
+
+// Insert a document into the 'classwork' collection
+db.classwork.insertOne({
+    "_id": 123,
+    "Emp_ID": "10025AE336",
+    "Personal_details": {
+        "First_Name": "Radhika",
+        "Last_Name": "Sharma",
+        "Date_Of_Birth": "1995-09-26"
+    },
+    "Contact": {
+        "e-mail": "radhika_sharma.123@gmail.com",
+        "phone": "9848022338"
+    },
+    "Address": {
+        "city": "Hyderabad",
+        "Area": "Madapur",}})
+/*OutPut:
+{
+  acknowledged: true,
+  insertedId: 123
+}*/
+
+// Create the collection named 'School'
+db.createCollection('School')
+// Insert a document into the 'School' collection
+db.School.insertOne([  
+  {
+    title: "Post Title 2",
+    body: "Body of post.",
+    category: "Event",
+    likes: 2,
+    tags: ["news", "events"],
+    date: Date()
+  }
+
+/* OutPut:
+ {
+  acknowledged: true,
+  insertedIds: {
+    '0': ObjectId('65b1553c5326bc7c7f57cf1c')
+    }
+}*/
+
+// Create a collection named 'Home'
+db.createCollection('Home')
+// Insert multiple documents into the 'Home' collection
+db.Home.insertMany([  
+  {
+    title: "Post Title 2",
+    body: "Body of post.",
+    category: "Event",
+    likes: 2,
+    tags: ["news", "events"],
+    date: Date()
+  },
+  {
+    title: "Post Title 3",
+    body: "Body of post.",
+    category: "Technology",
+    likes: 3,
+    tags: ["news", "events"],
+    date: Date()
+  },
+  {
+    title: "Post Title 4",
+    body: "Body of post.",
+    category: "Event",
+    likes: 4,
+    tags: ["news", "events"],
+    date: Date()
+  }
+])
+
+/*OutPut:
+    {
+  acknowledged: true,
+  insertedIds: {
+    '0': ObjectId('65b1553c5326bc7c7f57cf1c'),
+    '1': ObjectId('65b1553c5326bc7c7f57cf1d'),
+    '2': ObjectId('65b1553c5326bc7c7f57cf1e')
+  }
+}
+*/
