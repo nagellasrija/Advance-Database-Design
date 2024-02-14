4d558f85e92342ab4c2cf31395a0d04d2b233848
nagellasrija <94693223+nagellasrija@users.noreply.github.com>
2024-02-13 19:56:49 -0500

    Replace One.js

---
+++ Replace One.js
@@ -0,0 +1,26 @@
+// Replace document with specified _id with new document, including the _id field
+db.books.replaceOne(
+   {_id: ObjectId("65c3ae164d257fbeae952e2d")},
+   {
+     _id: ObjectId("65c3ae164d257fbeae952e2d"),
+     title: "Deep Dive into React Hooks",
+     ISBN: "0-3182-1299-4",
+     thumbnailUrl: "http://via.placeholder.com/640x360",
+     publicationDate: ISODate("2022-07-28T02:20:21.000Z"),
+     authors: ["Ada Lovelace"],
+   }
+ );
+
+// Replace document with specified _id with new document, excluding the _id field (implicitly creating a new ObjectId)
+ db.books.replaceOne(
+   {_id: ObjectId("65c3ae164d257fbeae952e2d")},
+   {
+     title: "Deep Dive into React Hooks",
+     ISBN: "0-3182-1299-6",
+     thumbnailUrl: "http://via.placeholder.com/640x360",
+     publicationDate: ISODate("2022-07-28T02:20:21.000Z"),
+     authors: ["Ada Lovelace"],
+   }
+ );
+ //To make sure we have the doccument
+ db.books.findOne({_id: ObjectId("65c3ae164d257fbeae952e2d")});
